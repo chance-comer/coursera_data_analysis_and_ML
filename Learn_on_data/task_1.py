c5a0cfaa84f3902a4b85816c6c07e1f6d6d7c949
Natalie <s4astlivii_4elovek@mail.ru>
2018-08-28 20:25:04 +0300

    part of notebook 1

--- Learn_on_data/task_1.py
+++ Learn_on_data/task_1.py
@@ -10,5 +10,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("weights_heights.csv")
-data.plot(y = 'Height', title = 'Height inch', color = 'blue', kind = 'hist' )
+data = pd.read_csv("weights_heights.csv", index_col = 'Index')
+#data.plot(y = 'Height', title = 'Height inch', color = 'blue', kind = 'hist' )
+
+def make_bmi(weight_pound, height_inch):
+    kg_to_pound, METER_TO_INCH =  2.20462, 39.37
+    return (weight_pound / kg_to_pound) / (height_inch / METER_TO_INCH) ** 2
+
+data['BMI'] = data.apply(lambda row: make_bmi(row['Weight'], row['Height']), axis = 1)
+
+#sns.pairplot(data)
+
+data.plot(x = 'Weight', y = 'Height', kind = 'scatter', title = 'Зависимость роста от веса')
+
+#plt.scatter(x = data['Weight'], y = data['Height'])
