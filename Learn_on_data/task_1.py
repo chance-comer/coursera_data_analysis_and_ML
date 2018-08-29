d6edb852bc21dc3ee8a11d8574ccb3abb417fef5
Natali <s4astlivii_4elovek@mail.ru>
2018-08-29 17:59:02 +0300

    program task

--- Learn_on_data/task_1.py
+++ Learn_on_data/task_1.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
+from mpl_toolkits.mplot3d import Axes3D
 
 data = pd.read_csv("weights_heights.csv", index_col = 'Index')
 #data.plot(y = 'Height', title = 'Height inch', color = 'blue', kind = 'hist' )
@@ -21,6 +23,45 @@ data['BMI'] = data.apply(lambda row: make_bmi(row['Weight'], row['Height']), axi
 
 #sns.pairplot(data)
 
-data.plot(x = 'Weight', y = 'Height', kind = 'scatter', title = 'Зависимость роста от веса')
+f = data.plot(x = 'Weight', y = 'Height', kind = 'scatter', title = 'Зависимость роста от веса')
 
 #plt.scatter(x = data['Weight'], y = data['Height'])
+def compute_error(w):  
+  y_hat = w[0] + w[1] * data['Weight']
+  error = (data['Height'] - y_hat) ** 2
+  return error.sum()
+
+#print(compute_error(1, 1))
+x = np.linspace(70, 170, len(data))
+plt.scatter(data['Weight'], data['Height'])
+plt.plot(x, 60 + 0.05 * x, color = 'orange')
+plt.plot(x, 50 + 0.16 * x, color = 'violet')
+plt.xlabel("Вес")
+plt.ylabel("Рост")
+plt.title("Случайные регрессионные прямые")
+plt.show()
+
+w1 = np.linspace(-5, 5, len(data))
+#error = [compute_error(50, w) for w in w1]
+#plt.plot(w1, error)
+res = opt.minimize_scalar(lambda x: compute_error([50, x]), bounds=(-5,5))
+
+fig = plt.figure()
+ax = fig.gca(projection='3d') # get current axis
+
+# Создаем массивы NumPy с координатами точек по осям X и У. 
+# Используем метод meshgrid, при котором по векторам координат 
+# создается матрица координат. Задаем нужную функцию Z(x, y).
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+Z = [[compute_error([w0, w1]) for w0 in X] for w1 in Y]
+X, Y = np.meshgrid(X, Y)
+# Наконец, используем метод *plot_surface* объекта 
+# типа Axes3DSubplot. Также подписываем оси.
+surf = ax.plot_surface(X, Y, Z)
+ax.set_xlabel('Intercept')
+ax.set_ylabel('Slope')
+ax.set_zlabel('Error')
+plt.show()
+
+answer = opt.minimize(compute_error, x0 = [0, 0], method = 'L-BFGS-B', bounds = [(-100, 100), (-5, 5)])
---
+++ Learn_on_data/week1_task_2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 29 17:01:02 2018
+
+@author: kazantseva
+"""
+
+import pandas as pd
+import numpy as np
+
+adver_data = pd.read_csv("advertising.csv")
+
+X = np.array([adver_data["TV"].values, adver_data["Radio"].values, adver_data["Newspaper"].values]) # Ваш код здесь
+y = np.array(adver_data["Sales"].values) # Ваш код здесь
+
+means, stds = X.mean(axis = 1), X.std(axis = 1)# Ваш код здесь
+
+X = (X - np.repeat(means, len(adver_data), axis = 0).reshape(3, len(adver_data))) / \
+    np.repeat(stds, len(adver_data), axis = 0).reshape(3, len(adver_data))
+
+X = X.T
+X = np.hstack((np.ones(len(X)).reshape(len(X), 1), X))
+
+#a = [[1, 2, 3], [4, 5, 6]]
+#o = np.ones(len(a)).reshape(len(a), 1)
+
+#b = np.hstack((o, a))
