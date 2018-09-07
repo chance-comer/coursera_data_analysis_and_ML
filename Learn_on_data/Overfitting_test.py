355daf505f3196c9c65e44ed2f044275ddea1349
Natali <s4astlivii_4elovek@mail.ru>
2018-09-07 15:31:33 +0300

    program task week 3

--- Learn_on_data/Overfitting_test.py
+++ Learn_on_data/Overfitting_test.py
@@ -10,3 +10,16 @@ import pandas as pd
 df = pd.read_csv('bikes_rent.csv')
 
 df.iloc[:,:-1].corrwith(df['cnt'])
+df[['temp', 'atemp', 'hum', 'windspeed(mph)', 'windspeed(ms)', 'cnt']].corr()
+
+from sklearn.preprocessing import StandardScaler
+
+data = [[0, 0], [0, 0], [1, 1], [1, 1]]
+scaler = StandardScaler()
+print(scaler.fit(data))
+
+print(scaler.mean_)
+
+print(scaler.transform(data))
+
+print(scaler.transform([[2, 2]]))
