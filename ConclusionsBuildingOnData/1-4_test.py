28e166d570b5fe0ca5911407d68e4893c522cc66
Natali <s4astlivii_4elovek@mail.ru>
2018-10-22 17:53:49 +0300

    hell

--- ConclusionsBuildingOnData/1-4_test.py
+++ ConclusionsBuildingOnData/1-4_test.py
@@ -7,17 +7,17 @@ Created on Wed Oct 17 16:08:25 2018
 
 import numpy as np
 import pandas as pd
-from scipy.stats import norm, binned_statistic_2d, chisquare
+from scipy.stats import norm, binned_statistic_2d, chisquare, binom_test
 from statsmodels.stats.proportion import proportion_confint, samplesize_confint_proportion
 
 # H0 - уровень стресса не отличается. Средние равны.
 p_75 = proportion_confint(75, 100, method = 'normal')
-p_60 = proportion_confint(60, 100, method = 'normal')
+p_60 = proportion_confint(67, 100, method = 'normal')
 
 count1 = 100
 count2 = 100
 
-s_count1 = 60
+s_count1 = 67
 s_count2 = 75
 
 p1 = s_count1 / count1
@@ -29,7 +29,9 @@ alpha = 0.05
 
 lvl = 2 * norm.sf(p_difference)
 
-z = norm.ppf(1 - alpha / 2)
+answer1 = binom_test(67, 100, .75, alternative = 'two-sided')
+
+z = norm.ppf(1 - 0.082222588913866079 / 2)
 
 p_difference_lb = p_difference - z * (p1 * (1 - p1) / s_count1 + p2 * (1 - p2) / s_count2) ** 0.5
 p_difference_rb = p_difference + z * (p1 * (1 - p1) / s_count1 + p2 * (1 - p2) / s_count2) ** 0.5
@@ -38,4 +40,4 @@ data = pd.read_csv('pines.txt', sep = '\t')
 
 hist = binned_statistic_2d(data.iloc[:, 0], data.iloc[:, 1], None, 'count', [5, 5])
 lf = hist.statistic.flatten()
-answer = chisquare(lf, [hist.statistic.flatten().sum() / 25] * len(lf))
+answer4 = chisquare(lf, [hist.statistic.flatten().sum() / 25] * len(lf))
---
+++ ConclusionsBuildingOnData/2-1_test.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 22 15:04:32 2018
+
+@author: kazantseva
+"""
+
+import scipy.stats as st
+import pandas as pd
+from sklearn import model_selection, linear_model, ensemble, metrics
+from statsmodels.stats.weightstats import *
+
+data = pd.read_csv('diamonds.txt', sep = '\t')
+
+m_gen = 9.5
+sigma = 0.4
+
+m_sample = 9.57
+sample_size = 160
+
+z = (9.57 - 9.5) / (sigma / sample_size**0.5)
+lvl = 2* (1 - st.norm.cdf(abs(z)))
+
+train_sample, test_sample = model_selection.train_test_split(data, test_size = 0.25, random_state = 1)
+
+train_y = train_sample['price']
+train_X = train_sample.drop(['price'], axis = 1)
+test_y = test_sample['price']
+test_X = test_sample.drop(['price'], axis = 1)
+
+lr_model = linear_model.LinearRegression()
+lr_model.fit(train_X, train_y)
+lr_preds = lr_model.predict(test_X)
+lr_score = metrics.mean_absolute_error(test_y, lr_preds)
+lr_dev = abs(lr_preds - test_y)
+
+rf_model = ensemble.RandomForestRegressor(random_state = 1)
+rf_model.fit(train_X, train_y)
+rf_preds = rf_model.predict(test_X)
+rf_score = metrics.mean_absolute_error(test_y, rf_preds)
+rf_dev = abs(rf_preds - test_y)
+
+t_val = st.ttest_rel(lr_dev, rf_dev)
+
+diff = lr_score - rf_score
+
+cm = CompareMeans(DescrStatsW(lr_dev), DescrStatsW(rf_dev))
